e15f910b2b1d427df24ea21f6b46eca7bf23137e
Abhishek Mishra <mishraabhi@gmail.com>
2021-07-25 10:19:14 +0530

    added home component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 // components
 import Header from './components/Header';
+import Home from './components/Home';
 // styles
 import { GlobalStyle } from './GlobalStyle'
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      I am up. 
+      <Home/>
       <GlobalStyle />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// API 
+import API from '../API';
 // config
-
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+
+// components
+
+// Hooks
+
+// images
+import NoImage from '../images/no_image.jpg';
+
+function Home() {
+  const [state, setState ] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+      const movies = await API.fetchMovies(searchTerm, page);
+      
+      setState(prev => ({
+        ...movies,
+        results: 
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results]  
+      }))
+
+    } catch(err) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  // Initial render
+  useEffect(() => {
+    fetchMovies(1)
+  }, []);
+
+  console.log(state);
+  return <div> Home Page </div>
+}
+
+export default Home;
